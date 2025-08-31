3f382d9c709b41412abc37c8b788e52f146e3761
kaedwen <kaedwen@heinrich.blue>
2025-08-31 17:29:06 +0200

    restructure and use cron
    Signed-off-by: kaedwen <kaedwen@heinrich.blue>

---
+++ cmd/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/kaedwen/speedtest/pkg/app"
+	"go.uber.org/zap"
+)
+
+func main() {
+	log, err := zap.NewDevelopment()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	app := app.NewApplication(log)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Hour)
+	defer cancel()
+
+	if err := app.Run(ctx); err != nil {
+		log.Fatal("failed to run application", zap.Error(err))
+	}
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	sig := <-sigs
+	log.Info("Received signal", zap.String("signal", sig.String()))
+
+	cancel()
+	app.Close()
+	log.Info("Application stopped")
+}
--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/influxdata/line-protocol v0.0.0-20210922203350-b1ad95c89adf // indirect
 	github.com/oapi-codegen/runtime v1.1.2 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/spf13/pflag v1.0.7 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/net v0.43.0 // indirect
